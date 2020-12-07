33c4e3d73205b5cd45a97a5aff5e34a8258fae3e
DinobergueViana <dinobergueviana@gmail.com>
2020-12-07 09:49:22 -0300

    criando arquivos para cadastro de novo usuario

---
+++ public/js/cadastro.js
@@ -0,0 +1,12 @@
+// capturando os elementos que serão tratados
+// e armazenando em vaariavéis
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const confirmacao = document.getElementById('confirmacao');
+const btnCadastrar = document.getElementById('btnCadastrar');
+const formCadastro = document.getElementById('formCadastro');
+
+// validar dados 
+
+// associar o clique do botão à chamada de uma função
+
